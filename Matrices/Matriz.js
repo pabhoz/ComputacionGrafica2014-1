82692757edc0d389172c8410dd614fe8b7f6a150
Pablo Bejarano <pabhoz@gmail.com>
2014-02-07 13:46:59 -0500

    Una ayudita para la tarea.

---
+++ Matrices/Matriz.js
@@ -0,0 +1,32 @@
+glMatrixArrayType = typeof Float32Array != "undefined" ? Float32Array : typeof WebGLFloatArray != "undefined" ? WebGLFloatArray : Array;
+
+var mat3 = {
+    create : function (a) {
+        var b = new glMatrixArrayType(9);
+        if (a) {
+            b[0] = a[0];
+            b[1] = a[1];
+            b[2] = a[2];
+            b[3] = a[3];
+            b[4] = a[4];
+            b[5] = a[5];
+            b[6] = a[6];
+            b[7] = a[7];
+            b[8] = a[8];
+            b[9] = a[9]
+        }
+        return b
+    },
+    set : function (a, b) {
+        b[0] = a[0];
+        b[1] = a[1];
+        b[2] = a[2];
+        b[3] = a[3];
+        b[4] = a[4];
+        b[5] = a[5];
+        b[6] = a[6];
+        b[7] = a[7];
+        b[8] = a[8];
+        return b
+    }
+};
